8cf9fe25067b3a0f9b4cd94133c1ec5824496bed
Paul Tirlisan <paultirlisan@takeofflabs.com>
2020-07-28 17:11:24 +0300

    Added rating statistics for each company and solved small bugs.

--- app/models/company.rb
+++ app/models/company.rb
@@ -9,4 +9,34 @@ class Company < ApplicationRecord
 				numericality: true
 	validates :headquarters, presence: true
 	validates :careers_advertisement, presence: true
+
+	def average_score
+		number_of_reviews == 0 ? 0 : total_score / number_of_reviews
+	end
+
+	def total_score
+		total_score = 0.0
+		self.ratings_frequency.each do |rating_frequency|
+			total_score += rating_frequency[0] * rating_frequency[1]
+		end
+		return total_score
+	end
+
+	def ratings_frequency
+		reviews.map(&:rating).map {|rating| rating.to_i}.group_by {|rating| rating}.map do |stars, vector|
+			[stars, vector.length]
+		end
+	end
+
+	def number_of_reviews
+		reviews.count
+	end
+
+	def reviews_with_rating(rating)
+		reviews.where(rating: rating).count
+	end
+
+	def percent_reviews_with_rating(rating)
+		number_of_reviews == 0 ? 0 : reviews_with_rating(rating) * 100 / number_of_reviews
+	end
 end
